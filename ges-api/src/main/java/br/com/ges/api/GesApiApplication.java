aa6feebee3810d5a2fc2393196ab2757d86173e2
felipe911 <felipe@20911@gmail.com>
2019-11-03 15:14:36 -0300

    Criando o projeto da API

---
+++ ges-api/src/main/java/br/com/ges/api/GesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ges.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesApiApplication.class, args);
+	}
+
+}
