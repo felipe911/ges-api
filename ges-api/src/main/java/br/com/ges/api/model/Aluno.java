3c0273a402669c36b7f1c6c1c244a0a5a4bdffc6
felipe911 <felipe@20911@gmail.com>
2019-11-03 16:10:08 -0300

    Adicionando model Aluno e Empresa

---
+++ ges-api/src/main/java/br/com/ges/api/model/Aluno.java
@@ -0,0 +1,149 @@
+package br.com.ges.api.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "aluno")
+public class Aluno {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@NotNull
+	private String nome;
+
+	@NotNull
+	private String ra;
+
+	@NotNull
+	private String curso;
+
+	@NotNull
+	private int semestre;
+
+	@NotNull
+	private int periodo;
+
+	@NotNull
+	private String email;
+
+	@NotNull
+	private String telefone;
+
+	@NotNull
+	private char sexo;
+
+	@NotNull
+	private LocalDate dataVestibular;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRa() {
+		return ra;
+	}
+
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public int getSemestre() {
+		return semestre;
+	}
+
+	public void setSemestre(int semestre) {
+		this.semestre = semestre;
+	}
+
+	public int getPeriodo() {
+		return periodo;
+	}
+
+	public void setPeriodo(int periodo) {
+		this.periodo = periodo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public LocalDate getDataVestibular() {
+		return dataVestibular;
+	}
+
+	public void setDataVestibular(LocalDate dataVestibular) {
+		this.dataVestibular = dataVestibular;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
---
+++ ges-api/src/main/java/br/com/ges/api/model/Empresa.java
@@ -0,0 +1,171 @@
+package br.com.ges.api.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@NotNull
+	private String razaoSocial;
+
+	@NotNull
+	private String contatoResponsavel;
+
+	@NotNull
+	private String endereco;
+
+	@NotNull
+	private String cep;
+
+	@NotNull
+	private String bairro;
+
+	@NotNull
+	private String cidade;
+
+	@NotNull
+	private char uf;
+
+	@NotNull
+	private int numero;
+
+	@NotNull
+	private String telefone;
+
+	@NotNull
+	private String email;
+
+	@NotNull
+	private LocalDate prazoConvenio;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getContatoResponsavel() {
+		return contatoResponsavel;
+	}
+
+	public void setContatoResponsavel(String contatoResponsavel) {
+		this.contatoResponsavel = contatoResponsavel;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public char getUf() {
+		return uf;
+	}
+
+	public void setUf(char uf) {
+		this.uf = uf;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getPrazoConvenio() {
+		return prazoConvenio;
+	}
+
+	public void setPrazoConvenio(LocalDate prazoConvenio) {
+		this.prazoConvenio = prazoConvenio;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empresa other = (Empresa) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
