56c87a85e72b7d9112c73f7a211c41a832869ce6
felipe <felipe@amee.com.br>
2019-11-05 10:37:39 -0300

    Adiciona operações de CRUD para Aluno

--- ges-api/src/main/java/br/com/ges/api/controller/AlunoController.java
+++ ges-api/src/main/java/br/com/ges/api/controller/AlunoController.java
@@ -1,8 +1,14 @@
 package br.com.ges.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +23,85 @@ public class AlunoController {
 	@Autowired
 	private AlunoService alunoService;
 	
+	/**
+	 * 
+	 * Lista todos Alunos
+	 * 
+	 */
+//	@ApiOperation(value = "Lista todos os registros de Alunos")
+//	@ApiResponses(value = { @ApiResponse(code = 200, message = "Sucesso", response = Aluno.class),
+//			@ApiResponse(code = 201, message = "Criado"), @ApiResponse(code = 204, message = "Sem conteúdo"),
+//			@ApiResponse(code = 401, message = "Sem autorização"), @ApiResponse(code = 403, message = "Proibido"),
+//			@ApiResponse(code = 404, message = "Não encontrado"),
+//			@ApiResponse(code = 500, message = "Erro interno no servidor") })
+	@GetMapping(produces = "application/json")
+	public List<Aluno> listar() {
+		return alunoService.listar();
+	}
 	
-//	@ApiOperation(value = "Exibe um registro de Aluno")
-//	@ApiResponses(value = @ApiResponse(code = 200, message = "Retorna um registro da entidade buscada", response = Aluno.class))
+	/**
+	 * 
+	 * Exibe o registro do aluno
+	 * 
+	 */
+//	@ApiOperation(value = "Exibe o registro do aluno")
+//	@ApiResponses(value = { @ApiResponse(code = 200, message = "Sucesso", response = Aluno.class),
+//			@ApiResponse(code = 201, message = "Criado"), @ApiResponse(code = 204, message = "Sem conteúdo"),
+//			@ApiResponse(code = 401, message = "Sem autorização"), @ApiResponse(code = 403, message = "Proibido"),
+//			@ApiResponse(code = 404, message = "Não encontrado"),
+//			@ApiResponse(code = 500, message = "Erro interno no servidor") })
 	@GetMapping("/{id}")
 	public Aluno exibir(@PathVariable Long id) throws BusinessException {
 		return alunoService.exibir(id);
+	}
+	
+	
+	/**
+	 * 
+	 * Salva uma novo Aluno
+	 * 
+	 */
+//	@ApiOperation(value = "Armazena um registro de Aluno")
+//	@ApiResponses(value = { @ApiResponse(code = 200, message = "Sucesso", response = Aluno.class),
+//			@ApiResponse(code = 201, message = "Criado"), @ApiResponse(code = 204, message = "Sem conteúdo"),
+//			@ApiResponse(code = 401, message = "Sem autorização"), @ApiResponse(code = 403, message = "Proibido"),
+//			@ApiResponse(code = 404, message = "Não encontrado"),
+//			@ApiResponse(code = 500, message = "Erro interno no servidor") })
+	@PostMapping()
+	public String salvar(@RequestBody Aluno aluno) throws BusinessException {
+		return alunoService.salvar(aluno);
+	}
+
+	
+	/**
+	 * 
+	 * Atualiza o registro de um Aluno a partir de seu ID
+	 *  
+	 */
+//	@ApiOperation(value = "Atualiza um registro de Aluno")
+//	@ApiResponses(value = { @ApiResponse(code = 200, message = "Sucesso", response = Aluno.class),
+//			@ApiResponse(code = 201, message = "Criado"), @ApiResponse(code = 204, message = "Sem conteúdo"),
+//			@ApiResponse(code = 401, message = "Sem autorização"), @ApiResponse(code = 403, message = "Proibido"),
+//			@ApiResponse(code = 404, message = "Não encontrado"),
+//			@ApiResponse(code = 500, message = "Erro interno no servidor") })
+	@PutMapping("/{id}")
+	public String atualizar(@PathVariable Long id, @RequestBody Aluno aluno) throws BusinessException {
+		return alunoService.atualizar(id, aluno);
+	}
+	
+	
+	/**
+	 * Deleta um registro Aluno caso ele exista
+	 * 
+	 */
+//	@ApiOperation(value = "Deleta um registro de Aluno")
+//	@ApiResponses(value = { @ApiResponse(code = 200, message = "Sucesso", response = Aluno.class),
+//			@ApiResponse(code = 201, message = "Criado"), @ApiResponse(code = 204, message = "Sem conteúdo"),
+//			@ApiResponse(code = 401, message = "Sem autorização"), @ApiResponse(code = 403, message = "Proibido"),
+//			@ApiResponse(code = 404, message = "Não encontrado"),
+//			@ApiResponse(code = 500, message = "Erro interno no servidor") })
+	@DeleteMapping("/{id}")
+	public String deletar(@PathVariable Long id) throws BusinessException {
+		return alunoService.deletar(id);
 	}
 }
--- ges-api/src/main/java/br/com/ges/api/service/AlunoService.java
+++ ges-api/src/main/java/br/com/ges/api/service/AlunoService.java
@@ -1,20 +1,71 @@
 package br.com.ges.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import br.com.ges.api.exception.BusinessException;
 import br.com.ges.api.model.Aluno;
 import br.com.ges.api.repository.AlunoRepository;
 
 @Service
 public class AlunoService {
-	
+
+	private static final String ANE = "Aluno não encontrado";
+	private String mensagem = null;
+
 	@Autowired
 	private AlunoRepository alunoRepository;
-	
+
 	public Aluno exibir(Long id) {
-		return alunoRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Aluno não encontrado"));
+		return alunoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(ANE));
+	}
+
+	public List<Aluno> listar() {
+		return (List<Aluno>) alunoRepository.findAll();
+	}
+
+	public String salvar(Aluno aluno) throws BusinessException {
+		
+		try {
+			alunoRepository.save(aluno);
+			this.mensagem = "Aluno registrado com sucesso.";
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return this.mensagem;
+	}
+
+	public String atualizar(Long id, Aluno aluno) {
+		
+		alunoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(ANE));
+		
+		try {
+			aluno.setId(id);
+			alunoRepository.save(aluno);
+			this.mensagem = "Aluno atualizado com sucesso.";
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return this.mensagem;
+	}
+
+	public String deletar(Long id) throws BusinessException {
+		alunoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(ANE));
+		
+		try {
+			alunoRepository.deleteById(id);
+			this.mensagem = "Aluno deletado com sucesso.";
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return this.mensagem;
 	}
 }
