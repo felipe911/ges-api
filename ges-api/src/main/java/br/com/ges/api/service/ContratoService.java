65c3b9bc39c6662d59557ee81df0dc22e057514e
felipe <felipe@amee.com.br>
2019-11-06 16:27:02 -0300

    Adiciona endpoints

--- ges-api/src/main/java/br/com/ges/api/service/ContratoService.java
+++ ges-api/src/main/java/br/com/ges/api/service/ContratoService.java
@@ -1,14 +1,14 @@
 package br.com.ges.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.ges.api.enums.StatusEnum;
 import br.com.ges.api.exception.BusinessException;
-import br.com.ges.api.model.Aluno;
 import br.com.ges.api.model.Contrato;
-import br.com.ges.api.model.Empresa;
 import br.com.ges.api.model.Estagio;
 import br.com.ges.api.repository.ContratoRepository;
 import br.com.ges.api.repository.EstagioRepository;
@@ -19,51 +19,88 @@ public class ContratoService {
 
 	private static final String CNE = "Contrato não encontrado.";
 	private String mensagem = null;
-	
+
 	@Autowired
 	private ContratoRepository contratoRepository;
-	
+
 	@Autowired
 	private EstagioRepository estagioRepository;
-	
+
 	public String associarContrato(AssociarContratoWrapper associarContrato) {
-		
+
 		Contrato contrato = new Contrato();
 		Estagio estagio = new Estagio();
-		
+
 		contrato = associarContrato.getContrato();
 		contrato.setEmpresa(associarContrato.getEmpresa());
 		estagio.setIdAluno(associarContrato.getAluno().getId());
-//		estagio.setIdContrato(associarContrato.get);
-//		estagio.setAluno(associarContrato.getAluno());
-//		estagio.setContrato(contrato);
-		
-		
+
 		Contrato contratoSalvo = contratoRepository.save(contrato);
 		Long id = contratoSalvo.getId();
 		estagio.setIdContrato(id);
 		estagio.setStatus(StatusEnum.ATIVO);
 		estagioRepository.save(estagio);
-		
-		
-		return this.mensagem = "Contrato Associado com sucesso.";
-		
+
+		return this.mensagem = "Contrato associado com sucesso.";
 	}
-	
+
 	public Contrato exibir(Long id) {
 		return contratoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(CNE));
 	}
-	
-	
-	public String salvar(Contrato contrato) throws BusinessException {
 
+	public List<Contrato> listar() {
+		return (List<Contrato>) contratoRepository.findAll();
+	}
 
-		// TODO: Verificar mensagem de erro retornada para o usuário.
+	public String deletar(Long id) throws BusinessException {
+		Contrato contratoEncontrado = contratoRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException(CNE));
 
-		contratoRepository.save(contrato);
-		this.mensagem = "Contrato registrado com sucesso.";
+		if (possuiEstagioAtivoNoContrato(contratoEncontrado)) {
+			return this.mensagem;
 
+		} else {
+			try {
+				contratoRepository.deleteById(id);
+				this.mensagem = "Contrato deletado com sucesso.";
+			} catch (Exception e) {
+				// TODO: Criar Exception correta
+			}
+		}
 		return this.mensagem;
 	}
-	
+
+	private boolean possuiEstagioAtivoNoContrato(Contrato contratoEncontrado) {
+
+		List<Estagio> estagios = estagioRepository.findAll();
+		estagios.removeIf(c -> c.getIdContrato() != contratoEncontrado.getId());
+
+		if (!estagios.isEmpty()) {
+			for (Estagio estagio : estagios) {
+				if (estagio.getStatus() == StatusEnum.ATIVO) {
+					this.mensagem = "Não é possivel deletar um contrato com estágio ativo";
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public String atualizar(AssociarContratoWrapper associarContrato) {
+
+		Contrato contrato = new Contrato();
+		Estagio estagio = new Estagio();
+
+		contrato = associarContrato.getContrato();
+		contrato.setEmpresa(associarContrato.getEmpresa());
+		estagio.setIdAluno(associarContrato.getAluno().getId());
+
+		Contrato contratoSalvo = contratoRepository.save(contrato);
+		Long id = contratoSalvo.getId();
+		estagio.setIdContrato(id);
+		estagio.setStatus(StatusEnum.ATIVO);
+		estagioRepository.save(estagio);
+
+		return this.mensagem = "Contrato atualizado com sucesso.";
+	}
 }
