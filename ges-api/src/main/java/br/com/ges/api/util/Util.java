3965bbdee9c956b491653ab9b0a343a446d629ff
felipe <felipe@amee.com.br>
2019-11-12 08:29:42 -0300

    Renomeia Util

--- ges-api/src/main/java/br/com/ges/api/util/Util.java
+++ ges-api/src/main/java/br/com/ges/api/util/Util.java
@@ -13,7 +13,8 @@ public class Util {
 	public static String localDateParaString(LocalDate localDate) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		String formatoString = localDate.format(formatter);
-		
+
 		return formatoString;
 	}
+
 }
